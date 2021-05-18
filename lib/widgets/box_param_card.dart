06ec6ac7b1108d5965af403dfc32182b5503726c
isaacmarcus <isaac.marcus.lam@gmail.com>
2021-05-18 18:05:25 +0800

    init

---
+++ lib/widgets/box_param_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class BoxParamCard extends StatefulWidget {
+  @override
+  _BoxParamCardState createState() => _BoxParamCardState();
+}
+
+class _BoxParamCardState extends State<BoxParamCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
